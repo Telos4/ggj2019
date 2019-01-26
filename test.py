20f5adaf50c9c9037dda7afbef005c423b7f8c71
ThePurox <28731015+ThePurox@users.noreply.github.com>
2019-01-26 22:49:13 +0100

    resize images

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ pygame.mixer.pre_init(44100,16,2,4096)
 pygame.mixer.init()
 pygame.init()
 pygame.mixer.music.load("Music/Hintergrundmusikfertig.ogg")
-pygame.mixer.music.play(-1)
+img = pygame.image.load("Art/generic_item_small.png")
+print(img.get_rect().size)
 time.sleep(5)
 
