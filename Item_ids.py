b276bfd6d1ee0404df064e5c51457189bd584bb4
ThePurox <28731015+ThePurox@users.noreply.github.com>
2019-01-26 11:10:11 +0100

    added id_dict and fixed inits

---
+++ Item_ids.py
@@ -0,0 +1,14 @@
+Roc_Para_id = 0
+Roc_Cont_id = 0
+Roc_Stor_id = 0
+Roc_Fuel_id = 0
+Mission_id = 0
+item_dict = {
+    "Roc_Parachute" : Roc_Para_id,
+    "Roc_Controll":Roc_Cont_id,
+    "Roc_Storage":Roc_Stor_id,
+    "Roc_Fuel":Roc_Fuel_id,
+    "Roc_Thruster":Roc_Thru_id,
+    "Mission":Mission_id,
+
+}
